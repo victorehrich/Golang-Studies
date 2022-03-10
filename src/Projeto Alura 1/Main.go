e1675a0d8da391bce32012f05a03617f375ddf41
victor medeiros <victormedeiro@yahoo.com.br>
2022-03-09 23:53:15 -0300

    finalizando projeto

--- src/Projeto Alura 1/Main.go
+++ src/Projeto Alura 1/Main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
@@ -25,6 +28,7 @@ func main() {
 			monitoramento()
 		case 2:
 			fmt.Println("Lista de logs:")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Finalizando programa. . .")
 			os.Exit(0)
@@ -38,7 +42,7 @@ func main() {
 
 func monitoramento() {
 	fmt.Println("Monitorando. . .")
-	sites := []string{"https://random-status-code.herokuapp.com/", "https://www.google.com/", "https://www.youtube.com/"}
+	sites := lerArquivo()
 	for i := 0; i < monitoramentos; i++ {
 		for i, site := range sites {
 			fmt.Println("Site", i+1, "-", site)
@@ -59,8 +63,10 @@ func testaSite(site string) {
 		holderStatusResp := strings.Trim(resp.Status, "200 ")
 		if strings.Split(strconv.Itoa(resp.StatusCode), "")[0] == "2" {
 			fmt.Println("Site:", site, "foi carregado com o status:", holderStatusResp)
+			registraLog(site, true, holderStatusResp)
 		} else {
 			fmt.Println("Site:", site, "esta com problemas; status: ", resp.Status)
+			registraLog(site, false, resp.Status)
 		}
 	}
 }
@@ -91,4 +97,47 @@ func consoleClear() {
 	cmd := exec.Command("cmd", "/c", "cls")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
+}
+
+func lerArquivo() []string {
+	var sites []string
+	//arquivo, err := ioutil.ReadFile("sites.txt")
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ops, ocorreu um erro ao acessar o arquivo", err)
+		os.Exit(-1)
+	}
+	leitor := bufio.NewReader(arquivo)
+	for {
+		site, err := leitor.ReadString('\n')
+		site = strings.TrimSpace(site)
+		sites = append(sites, site)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Ops, ocorreu um erro ao ler o arquivo", err)
+			os.Exit(-1)
+		}
+	}
+	arquivo.Close()
+	return sites
+}
+
+func registraLog(site string, status bool, statuscode string) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Ops, ocorreu um erro ao ler o arquivo", err)
+		os.Exit(-1)
+	}
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online:" + strconv.FormatBool(status) + " statuscode " + statuscode + "\n")
+	arquivo.Close()
+}
+func imprimeLogs() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("Ops, ocorreu um erro ao ler o arquivo", err)
+		os.Exit(-1)
+	}
+	fmt.Println(string(arquivo))
 }
